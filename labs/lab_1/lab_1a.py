876dab11b5dad0779143d0fc08fafd33c3d9d94f
MasteryThroughLearning <138404764+MasteryThroughLearning@users.noreply.github.com>
2026-02-16 18:16:46 -0600

    add personal introduction to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Carlos" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print(f"Hello everyone, my name is Carlos, and I live in Prosper, Texas.\nI love playing soccer for my school, and working on restoring my classic Mustang.\nI also participate in DECA and am the vice president of my school's Chinese Club.\nI am very excited to learn with you all!")
 
 if __name__ == "__main__":
     main()
